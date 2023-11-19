09db890d977ab7ce295ae2045af5a38e7c40b303
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2023-11-19 17:01:58 -0600

    Completed the non-weighted interval scheduling problem.

--- Non-Weighted Interval Scheduling Problem/non_weighted_problem.java
+++ Non-Weighted Interval Scheduling Problem/non_weighted_problem.java
@@ -1,3 +1,42 @@
-public class non_weighted_problem {
+import java.util.*;
 
+public class non_weighted_problem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numInstances = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numInstances; i++) {
+            int numJobs = scanner.nextInt();
+            int[][] mainArr = new int[numJobs][2];
+
+            for (int j = 0; j < numJobs; j++) {
+                mainArr[j][0] = scanner.nextInt();
+                mainArr[j][1] = scanner.nextInt();
+            }
+
+            Arrays.sort(mainArr, Comparator.comparingDouble(x -> x[1]));
+
+            System.out.println(helper(mainArr));
+
+            if (i < numInstances - 1) {
+                scanner.nextLine();
+            }
+        }
+        scanner.close();
+    }
+
+    private static int helper(int[][] intervals) {
+        int count = 0;
+        int lastEndTime = -1;
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] >= lastEndTime) {
+                count += 1;
+                lastEndTime = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
 }
